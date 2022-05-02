05d0f097ccc28820f1b87247bd89265ed0babfa4
Angel Avendaño <angel.aven.c@gmail.com>
2022-05-01 21:35:32 -0500

    Creating SVG component as Logo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { ListOfCategories } from './components/ListOfCategories'
 
 import { GlobalStyle } from './GlobalStyles'
 import { ListsOfPhotoCards } from './components/ListsOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
+    <Logo />
     <ListOfCategories />
     <ListsOfPhotoCards />
     <GlobalStyle />
