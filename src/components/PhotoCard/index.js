508a59bd3e77d8674694dad956ac8c1809039056
Angel Avendaño <angel.aven.c@gmail.com>
2022-05-29 00:49:56 -0500

    Using IntersectionObserver

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-import { ImgWrapper, Img, Button } from './styles'
+import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAE = 'https://images.unsplash.com/photo-1447684808650-354ae64db5b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const [show, setShow] = useState(false)
+  const ref = useRef(null)
 
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0]
+      console.log(isIntersecting)
+
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+
+    observer.observe(ref.current)
+  }, [ref])
+
+  return (
+    <Article ref={ref}>
+      {
+        show && (
+         <>
+           <a href={`/detail/${id}`}>
+             <ImgWrapper>
+               <Img src={src} />
+             </ImgWrapper>
+           </a>
+
+           <Button>
+             <MdFavoriteBorder size='32px' /> {likes} likes!
+           </Button>
+         </>
+        )
+      }
+    </Article>
   )
 }
