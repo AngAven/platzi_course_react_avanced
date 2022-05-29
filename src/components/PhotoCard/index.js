307b1b7d426359570c671349a7696f0e961cfcce
Angel Avendaño <angel.aven.c@gmail.com>
2022-05-29 11:15:49 -0500

    Working with localStorage and hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAE = 'https://images.unsplash.com/photo-1447684808650-354ae64db5b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAE }) => {
   const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (e) {
+      console.log(e)
+    }
+  })
   const ref = useRef(null)
 
   useEffect(() => {
@@ -28,6 +37,17 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAE }) => {
     })
   }, [ref])
 
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const SetLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <Article ref={ref}>
       {
@@ -39,8 +59,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAE }) => {
               </ImgWrapper>
             </a>
 
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes!
+            <Button onClick={() => SetLocalStorage(!liked)}>
+              <Icon size='32px' /> {likes} likes!
             </Button>
           </>
         )
