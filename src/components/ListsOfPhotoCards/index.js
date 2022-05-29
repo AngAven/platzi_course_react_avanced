affc0e50ba728df195c16af6b5d2fd6ed8fe39d6
Angel Avendaño <angel.aven.c@gmail.com>
2022-05-29 14:07:27 -0500

    Use Apollo with Graphql to filter by query

--- src/components/ListsOfPhotoCards/index.js
+++ src/components/ListsOfPhotoCards/index.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+
 import { PhotoCard } from '../PhotoCard'
 
-export const ListsOfPhotoCards = () => {
+const withPhotos = gql`
+query getPhotos{
+  photos{
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`
+
+const ListsOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(withPhotos)
+
+  if (error) {
+    return <h2>Internal Server Error</h2>
+  }
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
   return (
     <ul>
-      {
-        [1, 2, 3, 4, 5, 6, 7, 8].map(id => <PhotoCard key={id} id={id} />)
-      }
+      {data.photos.map(photo => (
+        <PhotoCard key={photo.id} {...photo} />
+      ))}
     </ul>
   )
 }
+
+export { ListsOfPhotoCards }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
 import { App } from './App'
 
-ReactDom.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-angel-platzi-cxl19b3wg-angaven.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'))
